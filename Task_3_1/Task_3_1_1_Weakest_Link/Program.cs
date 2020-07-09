679b27ecccc6d5c84aaad7d25186ba014e9670b1
Dmitriy Surovyagin <savourygin@gmail.com>
2020-07-09 17:47:36 +0400

    Add solutions to task 3.1

---
+++ Task_3_1/Task_3_1_1_Weakest_Link/Program.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Text;
+
+namespace Task_3_1_1_Weakest_Link
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! This program crosses out players one by one from the circle.");
+
+            bool done = false;
+
+            do
+            {
+                #region Dialog with user
+                Console.WriteLine("---------------------------------------");
+                Console.Write("The number of players: ");
+                string firstInput = Console.ReadLine().Trim();
+
+                bool success = int.TryParse(firstInput, out int number);
+                if (!success)
+                {
+                    Console.WriteLine("You entered an incorrect value. Try again!");
+                    continue;
+                }
+
+                if (number < 1)
+                {
+                    Console.WriteLine("The number of players cannot be less than one. Try again!");
+                    continue;
+                }
+                else if (number == 1)
+                {
+                    Console.WriteLine("Only one player? Well, you crossed yourself out. Try again!");
+                    continue;
+                }
+
+                Console.Write("The cross out number (type 0 for random): ");
+                string secondInput = Console.ReadLine().Trim();
+
+                success = int.TryParse(secondInput, out int toCrossOut);
+                if (!success)
+                {
+                    Console.WriteLine("You entered an incorrect value. Try again!");
+                    continue;
+                }
+
+                if (toCrossOut < 0)
+                {
+                    Console.WriteLine("You entered an incorrect value. Try again!");
+                    continue;
+                }
+                else if (toCrossOut > number)
+                {
+                    Console.WriteLine("You entered a number that exceeds the number of players. Try again!");
+                    continue;
+                }
+                else if (toCrossOut == 0)
+                {
+                    Random rand = new Random();
+                    toCrossOut = rand.Next(1, number);
+
+                }
+                #endregion
+
+                Console.WriteLine("The circle of {0} players is ready. \n" +
+                    "Start crossing out every {1}-th person.", number, toCrossOut);
+                
+                // Do all the work
+                DisplayResult(Counter(number, toCrossOut));
+                
+                Console.WriteLine("---------------------------------------");
+                Console.Write("Type 'yes' if you want to continue or something else to break: ");
+                string ans = Console.ReadLine().ToLower();
+
+                if (ans == "yes" || ans == "y")
+                {
+                    continue;
+                }
+                else
+                {
+                    done = true;
+                }
+
+            } while (!done);
+
+            Console.ReadKey();
+        }
+
+        /* The method creates an array of n natural numbers, 
+         * crosses out every k-th element from this array
+         * and retuns the the order in which elements were crossed out.
+         */
+        public static int[] Counter(int n, int k)
+        {
+            if (n <= 0 || k <= 0 || n < k)
+            {
+                throw new ArgumentException("One of the values you provided is incorrect. \n" +
+                    " Сannot create the required array.");
+            }
+
+            // Initialize all the variables
+            int[] positions = new int[n];
+            for (int i = 0; i < positions.Length; i++)
+            {
+                positions[i] = i + 1;
+            }
+
+            int total = n;
+            int pos = 0;
+            int quit = k;
+            int[] result = new int[n];
+
+            // Handle the special cases
+            if (k == 1)
+            {
+                return positions;
+            }
+            else if (k == n)
+            {
+                Array.Reverse(positions);
+                return positions;
+            }
+
+            // Start counting
+            do
+            {
+                // Save crossed out player to the result array
+                result[pos] = quit;
+                total--;
+                pos++;
+
+                // Recalculate crossed out number
+                quit += k;
+
+                if (quit > n)
+                {
+                    quit -= n;
+                }
+
+
+            } while (total != 0);
+
+            return result;
+        }
+
+        public static void DisplayResult(int[] order)
+        {
+            int remains = order.Length;
+            StringBuilder res = new StringBuilder();
+
+            for (int i = 0; i < order.Length; i++)
+            {
+                remains--;
+                Console.WriteLine("\t Round {0}. Crossed out {1}-th person. People left: {2}", i + 1, order[i], remains);
+                res.Insert(0, order[i].ToString() + "-th ");
+            }
+
+            Console.WriteLine("Done! It's impossible to cross out more people.");
+            Console.WriteLine("The final order: " + res);
+        }
+    }
+}
---
+++ Task_3_1/Task_3_1_2_Text_Analysis/Program.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Task_3_1_2_Text_Analysis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Dialog with the editor
+            Console.WriteLine("=================================================================\n" +
+                              "***  \\(^_^)/  Hello, dear fashion magazine editor!  \\(^_^)/   ***\n" +
+                              "*** This fancy program will check your text for originality.  ***\n" +
+                              "=================================================================");
+
+            bool done = false;
+
+            do
+            {
+                bool endEntering = false;
+                StringBuilder input = new StringBuilder();
+
+                // Request user text in parts
+                do
+                {
+                    Console.WriteLine("\t--- Enter a part of your text, please: ---");
+                    
+                    string rawText = Console.ReadLine().ToLower().Trim();
+                    input.Append(rawText + " ");
+
+                    Console.WriteLine("\t--- Type 'yes' if you wish to add more parts: ---");
+                    string answer = Console.ReadLine().ToLower().Trim();
+
+                    if (answer == "yes" || answer == "y")
+                    {
+                        continue;
+                    } 
+                    else
+                    {
+                        endEntering = true;
+                    }
+
+                } while (!endEntering);
+
+                if (input.Length <= 1)
+                {
+                    Console.WriteLine("The text you entered is too short. Try again!");
+                    continue;
+                }
+
+                #endregion
+
+                try
+                {
+                    Dictionary<string, int> result = new Dictionary<string, int>();
+                    result = WordCounter(input.ToString());
+                    DisplayResults(result);
+                } 
+                catch (Exception e)
+                {
+                    Console.Write(e.Message);
+                    Console.WriteLine(" Try again!");
+                    continue;
+                }             
+
+                #region Escape dialog
+                Console.WriteLine("-------------------------------------------------------------");
+                Console.Write("Type 'yes' if you want to continue or something else to break: ");
+                string ans = Console.ReadLine().ToLower().Trim();
+
+                if (ans == "yes" || ans == "y")
+                {
+                    continue;
+                }
+                else
+                {
+                    done = true;
+                }                
+
+            } while (!done);
+
+            Console.ReadKey();
+            #endregion
+        }
+
+        /* The method takes a string as input, breaks it into words, 
+         * counts the frequency of each word in the string, 
+         * and returns a dictionary <word: int>.       
+         */
+        public static Dictionary<string, int> WordCounter(string input)
+        {
+            // Split text into separate words
+            string[] words = input.Split(new[] { '.', ',', ' ', ';',
+                    ':', '!', '?', '"', '(', ')', '-'}, StringSplitOptions.RemoveEmptyEntries);
+
+            // Check for too short input
+            if (words.Length <= 3)
+            {
+                throw new ArgumentException("It appears your text contains too few words to check for word frequency.");
+            }
+
+            // Initiate dictionary to save each word and its frequency
+            Dictionary<string, int> wordDict = new Dictionary<string, int>();
+
+            // Check every word in input
+            foreach (string word in words)
+            {
+                // Skip articles, prepositions and other stop words
+                if (StopWords.englishWords.Contains(word))
+                {
+                    continue;
+                }
+
+                // If the word is in the dictionary, increase its frequency
+                if (wordDict.ContainsKey(word))
+                {
+                    wordDict[word] += 1;
+                }
+                else // Otherwise add this word to the dictionary as a new key
+                {
+                    wordDict.Add(word, 1);
+                }
+            }
+
+            return wordDict;
+        }
+
+        public static void DisplayResults(Dictionary<string, int> result)
+        {
+            Dictionary<string, int> commonWords = new Dictionary<string, int>();
+
+            // Scan the given dictionary
+            foreach (string word in result.Keys)
+            {
+                // Skip rare words
+                if (result[word] < 3)
+                {
+                    continue;
+                }
+                else // Add common words to a new dictionary
+                {
+                    commonWords.Add(word, result[word]);
+                }
+            }
+
+            // Print the most common words to the console
+            if (commonWords.Count < 1)
+            {
+                Console.WriteLine("----- Scanning the text -----");
+                Console.WriteLine("Your text is quite original. Frequent repetitions of words are not found!\n" +
+                    "Keep in mind that the program did not take into account the English articles, \n" +
+                    "prepositions and other stop words.");
+            }
+            else
+            {
+                Console.WriteLine("----- Scanning the text -----");
+                Console.WriteLine("The following words are repeated too often in your text: ");
+
+                foreach (string word in commonWords.Keys)
+                {
+                    Console.WriteLine("\t {0} : {1} times", word, commonWords[word]);
+                }
+
+                Console.WriteLine("Try to replace these words with synonyms.");
+            }
+        }
+    }   
+}
